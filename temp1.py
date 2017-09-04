2e982d83a52d8f314f622c8e559f0813d697ea20
Rodrigo Romero-de Lazaro <31290126+roderickromeoII@users.noreply.github.com>
2017-09-04 13:39:43 -0500

    2 axis graph

--- temp1.py
+++ temp1.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pylab as pl
 # Crea un vector (arreglo) con los valores del eje X
-x = [13,14,15,16,17,18,19,20,21]
+x = [2009,2010,2011,2012,2013,2014,2015,2016,2017]
 # Crea un vector (arreglo) con los valores en el eje Y para cada valor en el eje X
 y = [1,1,2,2,2,4,3,4,1]
 #Con esta isntruccion colocamos un titulo al eje Y de la grafica. 
 pl.ylabel('Novias y mascotas')
-pl.xlabel('tiempo')
+pl.xlabel('anios')
 # Grafica el vector x contra el vector y
-pl.plot(x,y)
+pl.axis ([2005,2020, 0,5])
+pl.plot(x,y, 'ro')
 #Guarda la grafica en el formato png
 pl.savefig('temp1.png')
+pl.show()
